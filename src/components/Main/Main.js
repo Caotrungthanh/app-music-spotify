bf730e565dbe0882020d9999db32531536f377da
Cao Trung Thành <37401313+Caotrungthanh@users.noreply.github.com>
2020-08-06 11:12:45 +0700

    Cập nhập thêm chức năng đăng xuất

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -17,31 +17,51 @@ export default class Main extends Component
         }
 
         this.state = {
-            loggedIn
+            loggedIn,
+            isDropdown: false,
         }
     }
+
+    onDropdown = () => {
+        this.setState({
+            isDropdown: !this.state.isDropdown
+        })
+    }
+
+    onLogout = () => {
+        localStorage.clear();
+        window.location.href = '/';
+    }
+
     render()
     {
         if(this.state.loggedIn === false)
         {
             return <Redirect to="/login"/>
         }
+        const menuClass = `dropdown-menu${this.state.isDropdown ? " show" : ""}`;
+            
         return (
             <div className="main">
                 <div className="upperNav">
-                    <div className="userDetail">
-                        <img src={this.props.userInfo.images[0].url} className="userImage" alt={this.props.userInfo.display_name}/>
-                        <p className="userName">
-                            {this.props.userInfo.display_name}
-                            <i class="fa fa-sort-down"></i>
-                        </p>
+                    <div className="dropdown" onClick={this.onDropdown}>
+                        <button type="button" className="userDetail btn" data-toggle="dropdown">
+                            <img src={this.props.userInfo.images[0].url} className="userImage" alt={this.props.userInfo.display_name}/>
+                            <p className="userName">
+                                {this.props.userInfo.display_name}
+                                <i className="fa fa-sort-down"></i>
+                            </p>
+                        </button>
+                        <div className={menuClass}>
+                            <a className="dropdown-item" href="/#" onClick={this.onLogout}>Đăng xuất</a>
+                        </div>
                     </div>
                 </div> 
                 <div className="container-fluid w-100">
                     <Switch>
                         <Route path="/" exact component={Categories}></Route>
                         <Route path="/search" component={Search}></Route>
-                        <Route path="/your-library">Lirbary</Route>
+                        <Route path="/your-library" >Lirbary</Route>
                         <Route path="/playlist/:id" component={PlaylistsDetails}></Route>
                     </Switch>
                 </div>
